7b56d39c0ee7b22be65e276a9ac5d8a8df1407a9
StefanKleinhans <107869712+StefanKleinhans@users.noreply.github.com>
2025-09-21 18:54:11 +0200

    Update api.py

--- app/api.py
+++ app/api.py
@@ -3,8 +3,18 @@ from models.base import Base
 from config.database import engine
 from endpoints import player
 from endpoints import auth
+from fastapi.middleware.cors import CORSMiddleware
+from config.settings import settings
 
 app = FastAPI(root_path="/app")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.CORS_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 
